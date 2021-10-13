32aadd6da8a44ba1ebde322ca89f206165adfc5b
Giovanni Della Dea <91167613+giossaurus@users.noreply.github.com>
2021-10-13 01:51:18 -0300

    Add files via upload

---
+++ testes_aulas/aula08a.py
@@ -0,0 +1,5 @@
+import math
+#from math import sqrt, floor
+num=int(input('Digite um numero: '))
+raiz=math.sqrt(num)
+print('A raiz quadrada de {} é igual a {:.2f}'.format(num, raiz))
---
+++ testes_aulas/aula11.py
@@ -0,0 +1,11 @@
+print('\033[0:30:41mOlá, mundo!\033[m')
+
+print('\033[4:33:44mOlá, mundo!\033[m')
+
+print('\033[1:35:43mOlá, mundo!\033[m')
+
+print('\033[30:42mOlá, mundo!\033[m')
+
+print('\033[mOlá, mundo!\033[m')
+
+print('\033[7:30mOlá, mundo!\033[m')
